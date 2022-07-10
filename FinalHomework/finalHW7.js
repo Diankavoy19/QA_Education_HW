8bb65cca7166027b47fa391899f34a0ed4750656
Diana Voitsekhovska <61407314+Diankavoy19@users.noreply.github.com>
2022-07-10 20:04:10 +0300

    Update finalHW7.js

--- FinalHomework/finalHW7.js
+++ FinalHomework/finalHW7.js
@@ -1,25 +1,25 @@
 const PageObj2 = require('../../pageobjects/PageObjectforswitchbutton.page.js');
 describe('Move to Explore GitHub, click to Topics button and displayed text there', () => {
-    xit.only('Load Main Page', async () => {
+    it.only('Load Main Page', async () => {
         await browser.url('https://github.com/');
         await browser.setWindowSize(1510, 900);
         await browser.pause(2000);
     });
-    xit.only('Move To Explore', async () => {
+    it.only('Move To Explore', async () => {
         await PageObj2.ExploreButton.moveTo(16,16);
         await browser.pause(4000);
         await PageObj2.clickExploreButton();
         await browser.pause(2000);
     });
-    xit.only('Move To Explore GitHub ', async () => {
+    it.only('Move To Explore GitHub ', async () => {
         await PageObj2.ExploreGithubButton.moveTo(16,16);
         await browser.pause(4000);
     });
-    xit.only('click to Explore GitHub button', async() => {
+    it.only('click to Explore GitHub button', async() => {
         await PageObj2.clickExploreGithubButton();
         await browser.pause(2000);
       });
-    xit.only('click to Topics button and check that text on the page is displayed', async() => {
+    it.only('click to Topics button and check that text on the page is displayed', async() => {
         await PageObj2.clickTopicsButton();
         await browser.pause(2000);
         let isDisplayed = await PageObj2.TopicsLabel.isDisplayed();
