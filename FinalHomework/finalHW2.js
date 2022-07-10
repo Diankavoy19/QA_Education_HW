22a55adb877e6893d810af4b01f7150fc513ae18
Diana Voitsekhovska <61407314+Diankavoy19@users.noreply.github.com>
2022-07-10 20:02:08 +0300

    Update finalHW2.js

--- FinalHomework/finalHW2.js
+++ FinalHomework/finalHW2.js
@@ -1,35 +1,35 @@
 const PageObj1 = require('../../pageobjects/PageObjectforFakeRegistaration2.page');
 const randGenerate = require('../randGenerate');
 describe('fake registration on another site', async() => {
-    xit.only(' Load page', async () => {
+    it.only(' Load page', async () => {
         await browser.setWindowSize(1510, 900);
         await browser.url('https://stepik.org/catalog?auth=registration');
         await browser.pause(1000);
     });
-    xit.only('generate username ', async() => {
+    it.only('generate username ', async() => {
         var username = await randGenerate.makeid(6);
         console.log("Generate username :" + username);
         await PageObj1.setUserName(username);
         await browser.pause(1000);
       });
-    xit.only('generate email', async() => {
+    it.only('generate email', async() => {
         var email = await randGenerate.getEmail();
         console.log("Generate email :" + email);
         await PageObj1.setEmail(email);
         await browser.pause(1000);
       });
-    xit.only('generate password ', async() => {
+    it.only('generate password ', async() => {
         var password = await randGenerate.makePass(12);
         console.log("Generate password :" + password);
         await PageObj1.setPassword(password);
         await browser.pause(1000);
       });
-    xit.only('click to Submit button', async() => {
+    it.only('click to Submit button', async() => {
         await PageObj1.clickSubmitButton();
         await browser.pause(2000);
         
       });
-    xit.only('chek url', async() => {
+    it.only('chek url', async() => {
       console.log('URL=' + await browser.getUrl());
       await expect(browser).toHaveUrlContaining('auth=registration');
       });
